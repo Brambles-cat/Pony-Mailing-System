9da3b13e1938bdb38111ecfb5613da90b1cf8a6f
Brambles-cat <gael.alejos05@gmail.com>
2024-05-29 22:14:51 -0700

    Properly getting form responses and can now send emails

--- Main.js
+++ Main.js
@@ -1,5 +1,70 @@
-function send_voting_results_email(cache_sheet_url) {
-  let items = [
+/**
+ * Processes every url that was voted for, then sends an email
+ * to the voter informing them of whether all their votes
+ * were valid, or pointing out which urls were problematic and
+ * suggests that they change their response so that every vote
+ * would count
+ * 
+ * Should not include any errors that were caused because of a
+ * programmers mistake. In this case, an error will be thrown after
+ * sending an email so as to make the problematic executions visible
+ * while not depriving the voter from confirmation that their votes
+ * are fine or should be edited
+ */
+function send_voting_results_email(event, cache_sheet_url) {
+  const response_items = event.response.getItemResponses();
+
+  // The line below should only be used for the type inference during development
+  // const response_items = FormApp.getActiveForm().getResponses()[0].getItemResponses()
+
+  const email = response_items.pop().getResponse()
+
+  // TODO: Use a regex to check whether the email can even be used
+  // The following should be tested before use
+  // ^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$
+  if (!email) {
+    Logger.log("No email in response")
+    return
+  }
+
+  const responses = response_items
+    .filter(response_item => response_item !== "")
+    .map(response_item => response_item.getResponse())
+
+  const ballot = Fetcher.fetchAll(responses, cache_sheet_url)
+  
+  const problematic = Checks.run_checks(ballot)
+
+  let subject, body
+
+  if (Object.keys(problematic).length) {
+    subject = "Votes Recieved With Issues D:"
+    body =
+      "Some issues were detected with your recent votes. Please ensure that all of them do not break the voting rules for each vote to count\n\nHere's a list of votes that had issues detected:"
+
+    body += Object.entries(problematic).reduce(
+      (message, [url, problems]) => message +=
+      `\n\n${url}\n${problems.join("\n")}`,
+      ""
+    )
+  }
+  else {
+    subject = "Thank You For Voting!"
+    body = "All of your votes appear to be valid, thank you for voting this month!"
+  }
+  
+  if (Preferences.v_send_emails)
+    GmailApp.sendEmail(email, subject, body)
+
+  Checks.throw_remaining_errs()
+}
+
+/**
+ * High likelihood of not doing anything or causing errors
+ * outside of development
+ */
+function test() {
+  let sample_response = [
     "www.youtube.com/watch?v=XO3AYMmh6-s",
     "https://www.youtube.com/watch?v=b0B_1YoZMY4",
     "https://www.youtube.com/watch?v=vcgBUBeLWSc",
@@ -61,19 +126,12 @@ function send_voting_results_email(cache_sheet_url) {
     "https://youtu.be/94OxnCnABZA?si=UBEiVnFy-4BXz4b4",
     "https://youtu.be/94OxnCnABZA?si=UBEiVnFy-4BXz4b4",
     "youtu.be/94OxnCnABZA?si=UBEiVnFy-4BXz4b4",
-    "youtu.be",
+    "youtu.be", // intentionally leaving in invalid links
     "www.youtube.com/",
-    "hbdubvufd"
-    ] // FormApp.getActiveForm().getItems(FormApp.ItemType.TEXT)
-  const email = items.pop()
+    "hbdubvufdss"
+  ]
 
-  ballot = Fetcher.fetchAll(items, cache_sheet_url)
-  
+  const ballot = Fetcher.fetchAll(sample_response, "https://docs.google.com/spreadsheets/d/18aHMyUMGM1z-pZYo4bQ6DrLP8qFQ5EuHSxyQNqZS2Hw/edit#gid=0")
+
   const invalid_votes = Checks.run_checks(ballot)
-
-  if (invalid_votes) {}
-}
-
-function m() {
-  send_voting_results_email("https://docs.google.com/spreadsheets/d/18aHMyUMGM1z-pZYo4bQ6DrLP8qFQ5EuHSxyQNqZS2Hw/edit#gid=0")
 }
