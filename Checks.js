1a50a940e455b6973deaf8ec82d0b46cdf1adfc0
Brambles-cat <gael.alejos05@gmail.com>
2024-06-08 16:54:44 -0700

    Now using email regex, annotations for video data, and implemented some checks

--- Checks.js
+++ Checks.js
@@ -1,20 +1,33 @@
 class Checks {
   static run_checks(ballot) {
+    const initial_vote_count = ballot.length
+
     // Duplicate check is outside of checks just so that
     // the ballot would be pruned of duplicates for the
     // remaining checks
-    ballot = this.check_duplicate(ballot)
+    ballot = this.check_duplicates(ballot)
 
     const checks = [
       this.check_blacklist,
       this.check_durations,
       this.check_upload_dates,
       this.check_uploader_diversity,
+
+      // Must be last since fail annotation should be applied
+      // to a vote with another annotation first if present
       this.check_uploader_occurences,
       // this.check_fuzzy
     ]
 
     checks.forEach(check => check(ballot))
+
+    let eligible_votes = ballot.length
+    eligible_votes -= ballot.reduce(
+      (sum, next_vote) => sum + (next_vote.annotations.length !== 0),
+      0
+    )
+
+    this.user_errs["Eligible votes:"] = [`${eligible_votes} / ${initial_vote_count}`]
     
     // compare ballot with no duplicates with user_errs for the 5 votes minimum check
     return this.user_errs
@@ -36,51 +49,52 @@ class Checks {
    */
   static report_err(url, e) {
     if (e.name === UserError.name)
-      this.user_errs[url] = [e.message]
+      this.user_errs[e.message] = [url]
     else
       this.other_errs.push(e)
   }
 
-  static check_fail(url, message) {
-    if (url in this.user_errs)
-      this.user_errs[url].push(message)
+  static check_fail(issue, problematic) {
+    if (issue in this.user_errs)
+      this.user_errs[issue].push(problematic)
     else
-      this.user_errs[url] = [message]
+      this.user_errs[issue] = [problematic]
   }
 
-  static check_duplicate(ballot) {
-    const occurences = [[ballot[0]]]
+  static check_duplicates(ballot) {
+    const sim_matrix = [[ballot[0]]]
     let found_similar
 
     for (let i = 1; i < ballot.length; ++i) {
       found_similar = false
       
-      for (var sim_array of occurences) {
+      for (const sim_array of sim_matrix) {
         if (this.same_video(ballot[i], sim_array[0])) {
           sim_array.push(ballot[i])
+          // ballot[i].annotations.push("duplicate")
+          // likely not needed
           found_similar = true
           break
         }
       }
 
       if (!found_similar)
-        occurences.push([ballot[i]])
+        sim_matrix.push([ballot[i]])
     }
 
-    occurences.forEach(sim_array => {
+    sim_matrix.forEach(sim_array => {
       if (sim_array.length > 1) {
-        var url = sim_array.shift().url
-
-        this.check_fail(url, `Potential duplicate${sim_array.length > 2 ? "s" : ""} found: ${
-          sim_array.reduce((result, next) => `${result}\n\t${next.url}`, "")
+        Checks.check_fail(
+          "Potential duplicates found:",`${
+          sim_array.reduce((result, next) => `${result}\n${next.url}`, sim_array.pop().url) + "\n"
         }`)
       }
     })
 
-    return occurences.map(sim_array => sim_array[0])
+    return sim_matrix.map(sim_array => sim_array[0])
   }
 
-  // TODO: Make this better
+  // TODO: Make this better, maybe alternate platform aliases should be considered
   static same_video(video1, video2) {
     return (
       video1.title    === video2.title    &&
@@ -91,13 +105,100 @@ class Checks {
 
   static check_blacklist(ballot) {}
 
-  static check_durations(ballot) {}
+  static check_durations(ballot) {
+    ballot.forEach(video_data => {
+      if (video_data.duration <= 30) {
+        video_data.annotations.push("too short")
+        Checks.check_fail("Too short:", video_data.url)
+      }
+      else if (video_data.duration <= 45) {
+        video_data.annotations.push("may be too short")
+        Checks.check_fail("May be too short:", video_data.url)
+      }
+    })
+  }
 
   static check_upload_dates(ballot) {}
 
-  static check_uploader_diversity(ballot) {}
+  static check_uploader_diversity(ballot) {
+    const s = new Set()
 
-  static check_uploader_occurences(ballot) {}
+    ballot.forEach(video_data => s.add(video_data.uploader))
+
+    if (s.size < 5) {
+      Checks.check_fail("Uploader Diversity:", `${s.size} out of a minimum of 5 different uploaders voted for`)
+      ballot.forEach(video_data => video_data.annotations.push("5 channel rule"))
+    }
+  }
+
+  static check_uploader_occurences(ballot) {
+    const occurences = new Map()
+
+    ballot.forEach(video_data => {
+      const initial_value = occurences.get(video_data.uploader)
+      occurences.set(video_data.uploader, initial_value === undefined ? 1 : initial_value + 1)
+    })
+
+    occurences.forEach((count, uploader) => {
+      if (count >= 3)
+        Checks.check_fail(
+          "More than 2 videos by the following uploaders have been voted for, which is not allowed:",
+          `${uploader}: ${count} votes`
+        )
+      else
+        occurences.delete(uploader)
+    })
+
+    const uploader_array = Array.from(occurences).map(entry => entry[0])
+
+    const rule_violating = ballot
+      .filter(video_data => occurences.has(video_data.uploader))
+      .sort((v1, v2) => uploader_array.indexOf(v1.uploader) - uploader_array.indexOf(v2.uploader))
+      // ^ ignoring really bad time complexity bc of the 10 vote limit
+
+    if (!rule_violating.length) return
+
+    let removal_candidates = []
+    let annotated_candidates = 0
+    let group_index = 0
+
+    // iterating over each uploader group to annotate videos to remove
+    // while the uploader limit is exceeded and with priority given
+    // to videos that are already annotated
+    for (const count of occurences.values()) {
+      const to_remove = count - 2
+
+      for (
+        let i = 0, relative_i;
+        i < count && annotated_candidates < to_remove;
+        ++i
+      ) {
+        relative_i = group_index + i
+
+        if (removal_candidates.length < to_remove) {
+          removal_candidates.push(rule_violating[relative_i])
+
+          if (rule_violating[relative_i].annotations.length)
+            ++annotated_candidates
+        }
+
+        else if (rule_violating[relative_i].annotations.length) {
+
+          removal_candidates[
+            removal_candidates.find(video_data => !video_data.annotations.length)
+          ] = rule_violating[relative_i]
+
+          ++annotated_candidates
+
+        }
+
+      }
+
+      removal_candidates.forEach(video_data => video_data.annotations.push("exceeds uploader occurence (not a rule violator annotaton)"))
+      removal_candidates = []
+      group_index += count
+    }
+  }
 }
 
 Checks.user_errs = {}
--- Main.js
+++ Main.js
@@ -11,7 +11,7 @@
  * while not depriving the voter from confirmation that their votes
  * are fine or should be edited
  */
-function send_voting_results_email(event, cache_sheet_url, test_input=null) {
+function send_voting_results_email(event, cache_sheet_url, ineligible_spreadsheet_url=null, test_input=null) {
   let response_items
   let responses, email
   
@@ -31,11 +31,8 @@ function send_voting_results_email(event, cache_sheet_url, test_input=null) {
       .filter(response_item => response_item !== "")
       .map(response_item => response_item.getResponse())
   }
-
-  // TODO: Use a regex to check whether the email can even be used
-  // The following should be tested before use
-  // ^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$
-  if (!email) {
+  
+  if (!email || !email.match("^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$")) {
     Logger.log("No email in response")
     return
   }
@@ -49,11 +46,11 @@ function send_voting_results_email(event, cache_sheet_url, test_input=null) {
   if (Object.keys(problematic).length) {
     subject = "Votes Recieved With Issues D:"
     body =
-      "Some issues were detected with your recent votes. Please ensure that all of them do not break the voting rules for each vote to count\n\nHere's a list of votes that had issues detected:"
+      "Some issues were detected with your recent votes. Please ensure that all of them follow the voting rules so all your votes count\n\nHere's a list of the detected issues:"
 
     body += Object.entries(problematic).reduce(
-      (message, [url, problems]) => message +=
-      `\n\n${url}\n${problems.join("\n")}`,
+      (message, [issue, problematic]) => message +=
+      `\n\n${issue}\n${problematic.join("\n")}`,
       ""
     )
   }
@@ -68,35 +65,46 @@ function send_voting_results_email(event, cache_sheet_url, test_input=null) {
   Checks.throw_remaining_errs()
 }
 
+function send_ineligible_video_email_announcement(ineligible_spreadsheet_url) {
+  Logger.log("not implemented")
+}
+
 /**
  * High likelihood of not doing anything or causing errors
  * outside of development
  */
 function test() {
   let sample_response = [
-    "www.youtube.com/watch?v=XO3AYMmh6-s",
-    "https://www.youtube.com/watch?v=b0B_1YoZMY4",
-    "https://www.youtube.com/watch?v=vcgBUBeLWSc",
-    "https://www.youtube.com/watch?v=5zQDgayNSGk",
-    "https://www.youtube.com/watch?v=4CbRYPVw7sA",
-    "https://www.youtube.com/watch?v=4WK5chqSwts",
-    "https://www.youtube.com/watch?v=hvF28l9M0OA",
-    "https://www.youtube.com/watch?v=J77Y-5In2fw",
-    "https://www.youtube.com/watch?v=4CbRYPVw7sA",
-    "https://www.youtube.com/watch?v=alSPaZfOrCg",
-    "https://www.youtube.com/watch?v=z0pGedFIL7k",
-    "https://www.youtube.com/watch?v=b0B_1YoZMY4",
-    "https://www.youtube.com/watch?v=5zQDgayNSGk",
-    "https://www.youtube.com/watch?v=vcgBUBeLWSc",
-    "https://www.youtube.com/watch?v=XO3AYMmh6-s",
-    "https://www.youtube.com/watch?v=f8NWlTYiafA",
-    "https://www.youtube.com/watch?v=alSPaZfOrCg",
-    "https://www.youtube.com/watch?v=4CbRYPVw7sA",
-    "https://www.youtube.com/watch?v=b7XTGxYCXNQ",
-    "https://www.youtube.com/watch?v=-QRlSf_Ffag",
-    "https://www.youtube.com/watch?v=cf57Xsw1K3Y",
-    "https://www.youtube.com/watch?v=lEvL-RMxRhY",
-    "https://www.youtube.com/watch?v=vcgBUBeLWSc",
+    "www.youtube.com/watch?v=bHB3XshiDc0",
+    "https://www.youtube.com/watch?v=q4qyeRAmC94",
+    "https://www.youtube.com/watch?v=SUvmldY-EDk",
+    "https://www.youtube.com/watch?v=4VKvCJ49gcY",
+    "https://www.youtube.com/watch?v=4VKvCJ49gcY",
+    "https://www.youtube.com/watch?v=ZPpTBzy3M2o",
+    "https://youtu.be/HaPXvPK-O3o?si=zJpEARYZf_GC0dAe",
+    "https://youtu.be/HaPXvPK-O3o?si=testingtest",
+    "https://www.youtube.com/watch?v=JYKEnxKFRh0",
+    "https://www.youtube.com/watch?v=B6mWSS2CSQ8",
+    "https://www.youtube.com/watch?v=o0T16Czgshg&list=LL&index=14",
+    "https://www.youtube.com/watch?v=NNla7ai3GLs&list=LL&index=50",
+    "https://www.youtube.com/watch?v=Qx2hokSdku8",
+    "https://youtu.be/HaPXvPK-O3o?si=zJpEARYZf_GC0dAe&test=test",
+    "https://www.youtube.com/watch?v=lfjEOQE7DxI",
+    "https://youtu.be",
+    "gael.alejos05@gmail.com"
+  ]
+
+  send_voting_results_email(null, "https://docs.google.com/spreadsheets/d/18aHMyUMGM1z-pZYo4bQ6DrLP8qFQ5EuHSxyQNqZS2Hw/edit#gid=0", null, sample_response)
+}
+
+
+
+
+/*
+"www.youtube.com/watch?v=XO3AYMmh6-s",
+    "https://www.youtube.com/watch?v=q4qyeRAmC94",
+    "https://www.youtube.com/watch?v=JYKEnxKFRh0",
+    "https://www.youtube.com/watch?v=SUvmldY-EDk",
     "https://pony.tube/w/mvc9HMTY4JGDyTp1nPN61N",
     "https://www.youtube.com/watch?v=b7XTGxYCXNQ",
     "https://www.youtube.com/watch?v=alSPaZfOrCg",
@@ -106,7 +114,7 @@ function test() {
     "https://www.youtube.com/watch?v=_v8vIcOYw2U",
     "https://www.youtube.com/watch?v=4WK5chqSwts",
     "https://www.youtube.com/watch?v=WEQq_D9TXM4",
-    "https://www.youtube.com/watch?v=XRHAAEbENbw",
+    "www.youtube.com/watch?v=XRHAAEbENbw",
     "https://www.bilibili.com/video/BV1UC4y1a76n/",
     "https://youtu.be/8wPr9DBlzrw?si=xJE8Yo2fZFUiwT3t",
     "https://www.bilibili.com/video/BV1Ts411D7XH/?spm_id_from=autoNext",
@@ -115,7 +123,7 @@ function test() {
     "https://youtu.be/vtQR6FGHHFw?si=qQcmbppYjem5O_sO",
     "https://www.youtube.com/watch?v=1FwtC-1bpx0&list=PLx2GQaX8C7Wgj6CWD7ZtAy3OL5jtvswZ1&index=3",
     "https://youtu.be/8wPr9DBlzrw?si=xJE8Yo2fZFUiwT3t",
-    "https://youtu.be/DL5e5-6CpAw?si=Hco0v4rpCKTZS6_e",
+    "youtu.be/DL5e5-6CpAw?si=Hco0v4rpCKTZS6_e",
     "https://youtu.be/HaPXvPK-O3o?si=zJpEARYZf_GC0dAe",
     "https://youtu.be/22V3rKriX60?si=e7C_W24i8yd37fDD",
     "https://youtu.be/Q-U-Jo9aWtE?si=YYaBy3nbk4oB4tO0",
@@ -126,7 +134,7 @@ function test() {
     "https://youtu.be/HaPXvPK-O3o?si=zJpEARYZf_GC0dAe",
     "https://youtu.be/HaPXvPK-O3o?si=zJpEARYZf_GC0dAe",
     "https://youtu.be/22V3rKriX60?si=e7C_W24i8yd37fDD",
-    "https://youtu.be/a5cmUcJ_8Co?si=81UvXkmnE7obRkL5",
+    "youtu.be/a5cmUcJ_8Co?si=81UvXkmnE7obRkL5",
     "https://www.dailymotion.com/video/x7kmdyb",
     "https://youtu.be/94OxnCnABZA?si=UBEiVnFy-4BXz4b4",
     "youtu.be/94OxnCnABZA?si=UBEiVnFy-4BXz4b4",
@@ -134,7 +142,4 @@ function test() {
     "youtu.be",
     "hbdubvufdss",
     "gael.alejos05@gmail.com"
-  ]
-
-  send_voting_results_email(null, "https://docs.google.com/spreadsheets/d/18aHMyUMGM1z-pZYo4bQ6DrLP8qFQ5EuHSxyQNqZS2Hw/edit#gid=0", sample_response)
-}
+    */
